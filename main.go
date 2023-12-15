7370be97fa1cd293735bf852dd6a3724eb6af4b4
YousefZinsazK78 <yousefzinsazk78@gmail.com>
2023-12-15 13:14:03 +0330

    add booleanAttr in template and edit main.go to show that...

--- main.go
+++ main.go
@@ -11,10 +11,12 @@ func main() {
 	helloComponent := templates.Hello("sina")
 	buttonComponent := templates.Button("Click Me!")
 	buttonComponent2 := templates.ButtonWithAttr("some value", "Click Me!")
+	booleanComponent := templates.BooleanAttr()
 
 	http.Handle("/", templ.Handler(helloComponent))
 	http.Handle("/btn", templ.Handler(buttonComponent))
 	http.Handle("/btnwvalue", templ.Handler(buttonComponent2))
+	http.Handle("/attr", templ.Handler(booleanComponent))
 
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		panic(err)
