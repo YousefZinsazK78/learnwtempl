9e06c6b59dc5f0abf684be55a3dda2646f070f34
YousefZinsazK78 <yousefzinsazk78@gmail.com>
2023-12-15 12:45:18 +0330

    add templ templates in project and generated with templ

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yousefzinsazk78/templ_test_project
 
 go 1.21.5
+
+require github.com/a-h/templ v0.2.476
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/yousefzinsazk78/templ_test_project/templates"
+)
 
 func main() {
 	fmt.Println("this is test from yousef...")
+	templates.Hello("yousef").Render(context.TODO(), os.Stdout)
 }
